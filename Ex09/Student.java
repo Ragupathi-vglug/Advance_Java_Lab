9a012f4eab21b95169af00d0f66c5325d9de4b6a
MADARA143390 <ragu26555@gmail.com>
2026-08-26 21:48:52 +0530

    Files added

--- Ex09/Student.java
+++ Ex09/Student.java
@@ -1,9 +1,6 @@
 import java.sql.*;
-
 public class Student {
-
     public static void main(String args[]) {
-
         try {
             // Load MySQL JDBC Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
---
+++ Ex10/SimpleBanner.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import java.applet.*;
+
+/*
+<applet code="SimpleBanner" width="700" height="50">
+</applet>
+*/
+
+public class SimpleBanner extends Applet implements Runnable {
+
+    String msg = " Accept Everything Nothing is Permanent.";
+    Thread t = null;
+    int state;
+    boolean stopFlag;
+
+    // Set colors and initialize thread
+    public void init() {
+        setBackground(Color.blue);
+        setForeground(Color.white);
+    }
+
+    // Start thread
+    public void start() {
+        t = new Thread(this);
+        stopFlag = false;
+        t.start();
+    }
+
+    // Entry point for the thread that runs the banner
+    public void run() {
+
+        char ch;
+
+        // Display banner
+        for (;;) {
+            try {
+                repaint();
+                Thread.sleep(250);
+
+                ch = msg.charAt(0);
+                msg = msg.substring(1, msg.length());
+                msg += ch;
+
+                if (stopFlag)
+                    break;
+
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    // Pause the banner
+    public void stop() {
+        stopFlag = true;
+        t = null;
+    }
+
+    // Display the banner
+    public void paint(Graphics g) {
+        g.drawString(msg, 500, 300);
+    }
+}
